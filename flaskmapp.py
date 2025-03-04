03f2504ae7ffdea2b9312bf2785f3023882ba512
Noah <noah.tingbratt@gmail.com>
2025-03-04 17:52:03 +0100

    Update flaskmapp.py

--- flaskmapp.py
+++ flaskmapp.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/j', methods=['GET'])
 def home():
     return jsonify({"message": "Welcome to the simple API!"})
 
